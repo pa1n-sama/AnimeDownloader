bc7fb7a0e27bf2e7f16bb76cd4cb20530d1ce0d5
Pain-sama <145512477+pa1n-sama@users.noreply.github.com>
2024-11-14 12:07:10 +0100

    optimizing  animedownload.py
    make the browser open in a separate  thread while waiting for the user input

--- animedownload.py
+++ animedownload.py
@@ -1,33 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome import options
+from selenium.webdriver.firefox import options
 from bs4 import BeautifulSoup
 from threading import Thread
 import requests
 import os 
+import time
 
 class Anime:
     def __init__(self):
-        op = options.Options()
-        op.add_argument("--headless")
-        op.add_argument("--disablegpu")
-        self.drive = webdriver.Chrome(options=op)
-        os.system("clear")
-        Thread(target=lambda:self.drive.get("https://www.google.com")).start()
+        self.wait = True 
+        Thread(target=self.prerun,daemon=True).start()
         
-        self.search_name = input("type the Your anime name: ").replace(" ","+")
-        os.system("clear")
-
-
+        self.search_name = input("\033ctype the Your anime name: ").replace(" ","+")
 
         self.llinks = []
         self.names=[]
+
+    def prerun(self):
+        try:
+            op = options.Options()
+            op.add_argument("--headless")
+            op.add_argument("--disablegpu")
+            self.drive = webdriver.Firefox(options=op)
+            self.drive.get("https://www.google.com")
+            self.wait = False
+        except:
+            print("\033cError: Check your internet connection")
+            exit()
     
     def getre(self,url):
         responce = requests.get(url)
         return BeautifulSoup(responce.text,"html.parser")
     
     def animepage(self):
+        print("\033c")
         for i in range(1,3):
             soup = self.getre(f"https://anime3rb.com/search?q={self.search_name}&page={i}")
             
@@ -39,12 +46,11 @@ class Anime:
                 print(f"{j}: {titles[j].get_text()}")
 
         if not len(self.llinks):
-            print("Name not found, please try again.")
+            print("\033cName not found, please try again.")
             exit()
-        num = int(input("Type the number of the anime you want to download: "))
-        os.system("clear")
+        num = int(input("\nType the number of the anime you want to download: "))
         self.anime_name = self.names[num]
-        self.Download_Directory= self.anime_name
+        self.Download_Directory= f"/home/pain/Downloads/Videos/{self.anime_name}"
 
         try:
             os.mkdir(self.Download_Directory)
@@ -53,10 +59,14 @@ class Anime:
         return self.llinks[num]
 
     def getdownloadlink(self):
+        while self.wait:
+            for i in [" ",".","..","..."]:
+                print("\033cPlease Wait"+i)
+                time.sleep(0.5)
         link = self.animepage().replace("titles","episode")
-        check = input("Are you sure you want to download all the anime?(YES/no):")
-        os.system("clear")
-        
+        check = input("\033cAre you sure you want to download all the anime?(YES/no):")
+        print("\033c")
+
         if check.lower() not in "no" or check == "":
             episode_num = 1
             with open("links.txt",'a') as file:
