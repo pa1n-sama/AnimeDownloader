3048eb7b208f9dba75c04dabc25b3ada82b80cdf
Pain-sama <145512477+pa1n-sama@users.noreply.github.com>
2024-12-08 07:47:15 +0100

    Update animedownload.py

--- animedownload.py
+++ animedownload.py
@@ -68,13 +68,19 @@ class Anime:
         print("\033c")
 
         if check.lower() not in "no" or check == "":
-            episode_num = 1
-            with open("links.txt",'a') as file:
-                while True:
+            
+            episode_num = 100
+            while True:
+                if f"{self.anime_name}-ep{episode_num}.mp4" in os.listdir(self.Download_Directory):
+                    print(f"\033c{self.anime_name}-ep{episode_num}.mp4 exist, skip to the next episode ...")
+                else:
                     try:
                         self.drive.get(link+"/"+str(episode_num))
                         allresolutions = self.drive.find_elements(By.CSS_SELECTOR,"a[class='focus:outline-none inline-block rounded-lg text-gray-700 hover:text-gray-900 focus:text-gray-900 dark:text-gray-200 dark:hover:text-gray-100 dark:focus:text-gray-100 px-4 py-2  bg-white dark:bg-dark-700 hover:bg-gray-50 dark:bg-dark-700 dark:hover:brightness-95 focus:bg-gray-50 dark:focus:brightness-95 active:bg-gray-200 dark:active:brightness-95 shadow-sm !rounded-none !px-8 !py-4 dark:!bg-dark-600/30']")
-                        
+                        if (not allresolutions): 
+                            print("episodes not found")
+                            self.drive.quit()
+                            exit()
                         for i in range(len(allresolutions)-1):
                             one = float(allresolutions[i].text.strip(" ميغابايت[]تحميل مباشر "))
                             second = float(allresolutions[i+1].text.strip(" ميغابايت[]تحميل مباشر "))
@@ -82,16 +88,17 @@ class Anime:
                                 third = allresolutions[i+1]
                                 allresolutions[i+1]= allresolutions[i]
                                 allresolutions[i] = third
-    
+
                         resolution = allresolutions[0].get_attribute("href")
+                        print("test")
                         
-                    except:
+                    except Exception as ec:
                         break
                     os.system(f'wget -O "{self.Download_Directory}/{self.anime_name}-ep{episode_num}.mp4" "{resolution}"')
-                    file.write(resolution+"\n")
-                    episode_num+=1
+                episode_num+=1
 
 
 
 x=Anime()
 x.getdownloadlink()
+x.drive.quit()
